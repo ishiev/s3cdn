10512902be7a7656402d69a56b5149bf0c7bb7c7
Nikolay Ishiev <nikolay.ishiev@gmail.com>
2023-05-23 23:59:47 +0300

    Add optimized file deletion procedure for housekeeper

--- src/housekeeper.rs
+++ src/housekeeper.rs
@@ -1,6 +1,7 @@
 use std::{
     path::{PathBuf, Path}, 
-    collections::BinaryHeap, 
+    collections::{BinaryHeap, BTreeMap}, 
+    ops::Bound::{Included, Unbounded},
     time::{SystemTime, Duration},
     cmp::Reverse, 
     fs::remove_file, 
@@ -23,7 +24,7 @@ pub struct ConfigHousekeeper {
     pub keeper_interval: Option<u64>,   // start interval in seconds, default 3600
 }
 
-/// Content storage entry 
+/// Content storage entry, ordered by time
 /// Field ordering is important!
 #[derive(PartialEq, Eq, PartialOrd, Ord)]
 struct TimeOrdEntry {
@@ -50,18 +51,25 @@ impl From<DirEntry> for TimeOrdEntry {
     }
 }
 
+// Oldest entry first collection
+type OldestFirstHeap = BinaryHeap<Reverse<TimeOrdEntry>>;
+
+/// Main housekeeper function
+/// (run in blocking context)
 fn housekeep(path: &Path, max_size: u64, _max_duration: Option<Duration>) {
     // walk content dir tree and collect files
     let walkdir = WalkDir::new(path).follow_links(true);
     let mut files = BinaryHeap::new();
     
     debug!("Housekeeper: start checking cache storage...");
+    let mut dir_count = 0u64;
     let mut dir_size = 0u64;
     for entry in walkdir {
         match entry {
             Ok(entry) if entry.file_type().is_file() => {
                 let entry = TimeOrdEntry::from(entry);
                 debug!("Houkeeper: found {:?}, size: {}, time: {:?}", entry.path, entry.size, entry.time);
+                dir_count += 1;
                 dir_size += entry.size;
                 // sort min first
                 files.push(Reverse(entry))
@@ -72,31 +80,68 @@ fn housekeep(path: &Path, max_size: u64, _max_duration: Option<Duration>) {
             }
         }
     }
-    debug!("Housekeeper: found {} files, storage size {} bytes", files.len(), dir_size);
+    debug!("Housekeeper: found {dir_count} files, storage size {dir_size} bytes");
 
-    // check directory size and clean up 
-    // until size is less than or equal to max_size
-    let mut removed_count = 0u64;
-    let mut removed_size = 0u64; 
-    while dir_size > max_size {
-        let Some(entry) = files.pop().map(|x| x.0) else { 
-            break
-        };
-        if let Err(e) = remove_file(&entry.path) {
-            error!("Housekeeper: error removing cache file {:?}: {e}", &entry.path);
-        } else {
-            removed_count += 1;
-            removed_size += entry.size;
-            dir_size -= entry.size;
-        }
-    }
+    // phase 2: check directory size and clean up 
+    let size_to_remove = dir_size.saturating_sub(max_size);
+    let (removed_count,  removed_size) = remove_to_size(&mut files, size_to_remove);
+
     if removed_count == 0 {
         debug!("Housekeeper: complete, nothing has been done.");
         return;
     }
     debug!("Housekeeper: removed {removed_count} files, {removed_size} bytes");
-    debug!("Housekeeper: complete, now we have {} files, storage size {} bytes", files.len(), dir_size);
+    debug!("Housekeeper: complete, now we have {} files, storage size {} bytes", 
+        dir_count - removed_count, 
+        dir_size - removed_size
+    );
 }
+
+/// Optimized file deletion procedure
+/// removes the minimum number of oldest files 
+fn remove_to_size(files: &mut OldestFirstHeap, mut size_to_remove: u64) -> (u64, u64) {
+    let mut size_tree = BTreeMap::new();
+    let mut selected = 0u64;
+    // select files from heap until given size and put it into b-tree map
+    // tree key is sum of file sizes
+    while selected < size_to_remove {
+        let Some(entry) = files.pop().map(|x| x.0) 
+        else { break };
+        selected += entry.size;
+        size_tree.insert(selected, entry);
+    }
+    // remove files until size_to_removed
+    let mut removed_count = 0u64;
+    let mut removed_size = 0u64;
+    while size_to_remove > 0 {
+        let key = { 
+            // select file to remove with key equal or greater than size_to_remove
+            let Some(entry) = size_tree
+                .range((Included(size_to_remove), Unbounded))
+                .next() 
+            else { break };
+            // remove file
+            if let Err(e) = remove_file(&entry.1.path) {
+                error!("Housekeeper: error removing cache file {:?}: {e}", &entry.1.path);
+            } else {
+                // calculate statictics
+                removed_count += 1;
+                removed_size += entry.1.size;
+                // calculate rest to remove
+                size_to_remove = size_to_remove.saturating_sub(entry.1.size);
+            }
+            // key value
+            *entry.0
+        };
+        // remove selected file from tree
+        // to prevent infinite loop in case of file removing errors
+        // in this case other file may be selected in next interation
+        size_tree.remove(&key);
+    }
+    // return stat counters
+    (removed_count, removed_size)
+}
+
 
 #[derive(Debug)]
 pub struct Housekeeper {
